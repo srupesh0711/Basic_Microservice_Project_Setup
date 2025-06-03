63d4889c75cd8a05c7ff6e585a106608a814ce1d
rupesh <sadirupeshr@gmail.com>
2025-06-03 09:41:15 +0530

    secured microservice(Authservice) using API gateway

--- authservice/src/main/java/com/authservice/AuthserviceApplication.java
+++ authservice/src/main/java/com/authservice/AuthserviceApplication.java
@@ -2,9 +2,11 @@ package com.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthserviceApplication {
 
 	public static void main(String[] args) {
